42c60bde75ad0a5f60c7ab888577a7dcace96fdf
Alexander Noskov <alexaner199513@yandex.ru>
2024-10-04 21:54:18 +0600

    Расширен уровень доступа у методов класса Managers

--- src/Managers.java
+++ src/Managers.java
@@ -1,10 +1,10 @@
 public class Managers {
 
-    static TaskManager getDefault() {
+    public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
 
-    static HistoryManager getDefaultHistory() {
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 
