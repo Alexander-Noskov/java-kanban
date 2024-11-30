54ec1dc4b292ca8f1e5223c363d64db6d8c8e05f
Alexander Noskov <alexaner199513@yandex.ru>
2024-12-01 02:22:27 +0600

    Создать базовый абстрактный класс для хранения общих методов и полей

---
+++ src/BaseHttpHandler.java
@@ -0,0 +1,50 @@
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public abstract class BaseHttpHandler {
+    protected final Gson gson = HttpTaskServer.getGson();
+    protected final TaskManager manager = HttpTaskServer.getTaskManager();
+
+
+    protected int getId(HttpExchange exchange) throws NumberFormatException {
+        String[] pathParts = exchange.getRequestURI().getPath().split("/");
+        return Integer.parseInt(pathParts[2]);
+    }
+
+    protected String getBody(HttpExchange exchange) throws IOException {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        }
+        return resultStringBuilder.toString();
+    }
+
+    protected void sendText(HttpExchange h, String text, int rCode) throws IOException {
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        h.sendResponseHeaders(rCode, resp.length);
+        h.getResponseBody().write(resp);
+        h.close();
+    }
+
+    protected void sendNotFound(HttpExchange h) throws IOException {
+        h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        h.sendResponseHeaders(404, 0);
+        h.close();
+    }
+
+    protected void sendHasInteractions(HttpExchange h) throws IOException {
+        h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        h.sendResponseHeaders(406, 0);
+        h.close();
+    }
+}
